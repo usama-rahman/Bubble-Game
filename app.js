c3cc2957e7d92212ad9f2cac491ca2aa7844911f
Usama Rahman <rahmanusama390@gmail.com>
2023-10-14 23:43:57 +0600

    Working

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// Start
+
+let clutter = "";
+
+for ( let i = 1; i <= 168; i++){
+   clutter += `<div class="bubble">5</div>`;
+}
+
+document.querySelector(".panel-bottom").innerHTML = clutter;
+
+
+
+
+
+
+
+
+
+
+
