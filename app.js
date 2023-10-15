c99f7a37f1dc77df568b85002cafeccd318f76f3
Usama Rahman <rahmanusama390@gmail.com>
2023-10-15 06:30:11 +0600

    :100:

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 
-let clutter = "";
-
-for ( let i = 1; i <= 168; i++){
-   clutter += `<div class="bubble">5</div>`;
+function makeBubble(){
+   let clutter = "";
+   for ( let i = 1; i <= 168; i++){
+      let random = Math.floor(Math.random() * 10)
+      clutter += `<div class="bubble">${random}</div>`;
+   }
+   document.querySelector(".panel-bottom").innerHTML = clutter;
 }
 
-document.querySelector(".panel-bottom").innerHTML = clutter;
+
+
+function runTime (){
+   let timer = 60;
+   let timeInterval = setInterval(function(){
+      if(timer>0){
+         timer--;
+         document.getElementById('timer').textContent = timer;
+      }else{
+         clearInterval(timeInterval);
+      }
+   },1000)
+}
+
+runTime();
+makeBubble();
+
+
 
 
 
