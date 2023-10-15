7f7c5b8570febd608ee10428a52e5a10abea21e7
Usama Rahman <rahmanusama390@gmail.com>
2023-10-15 16:02:22 +0600

    Dome 🍉

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
+let randomHit ;
+let timer = 50;
+let score = 0;
 
 function makeBubble(){
-   let clutter = "";
-   for ( let i = 1; i <= 168; i++){
+   var clutter = "";
+   for ( let i = 1; i <= 189; i++){
       let random = Math.floor(Math.random() * 10)
       clutter += `<div class="bubble">${random}</div>`;
    }
    document.querySelector(".panel-bottom").innerHTML = clutter;
 }
 
+// runtime
+
 function runTime(){
-   let timer = 60;
    let timeInterval = setInterval(function(){
       if(timer>0){
          timer--;
          document.getElementById('timer').textContent = timer;
       }else{
          clearInterval(timeInterval);
+         document.querySelector(".panel-bottom").innerHTML = `<h1>Game Over</h1>`;
       }
    },1000)
 }
 
+// getNewHit
+
 function getNewHit(){
-   let randomHit = Math.floor(Math.random() * 10);
+   randomHit = Math.floor(Math.random() * 10);
    document.querySelector('#hit').textContent = randomHit;
 }
 
+// increaseScore
+
 function increaseScore (){
-   let score = 2;
    score += 10;
    document.querySelector('#score').textContent = score;
 }
 
-increaseScore();
+const panelBottom = document.querySelector(".panel-bottom");
+
+panelBottom.addEventListener('click',function(det){
+   const clickedNum = Number(det.target.textContent);
+   if(randomHit === clickedNum){
+      increaseScore();  
+      getNewHit();
+      makeBubble(); 
+   }
+})
+
 getNewHit();
+makeBubble(); 
 runTime();
-makeBubble();
 
 
 
